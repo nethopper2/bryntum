97bdad2e3aab1afd5181859a84f1b61671fe4b7c
Shawn Makinson <smakinson@gmail.com>
2022-07-01 11:54:44 -0400

    added a comment

--- vuecli/src/components/TagsRenderer.vue
+++ vuecli/src/components/TagsRenderer.vue
@@ -1,4 +1,9 @@
 <template>
+  <!--
+  We also needed to add this span wrapper to fix the issues when using our component.
+  Vue3 does not require single root nodes, so we initially did not have the wrapping span.
+  If this is a hard requirement, the docs could reflect this when you get to that part of them.
+  -->
   <span>
     <span v-for="i in props.tags" :key="i" style="background: blue; margin: 3px; padding: 2px; color: white;">{{ i }}</span>
   </span>
